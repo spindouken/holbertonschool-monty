761fc9f2494c8667fbfc3dd4295c569dcb2a8a98
spindouken <spindouken@gmail.com>
2022-11-19 15:53:51 -0600

    update push function

--- push and pall.c
+++ push and pall.c
@@ -1,24 +1,33 @@
 #include "lists.h"
 
 /**
- * monty_push - add to top of stack
+ * monty_push - add new node to top of
+ * stack_t stack (doubly linked list)
  * @stack: stack
- * @line_number: 
- * Return: the address of the new element, or NULL on failure
+ * @line_number: line number
+ * Return: void
  */
 
-void monty_push(stack_t **stack, unsinged int line_number)
+void monty_push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
+	stack_t *node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	node->n = ;
+	node->prev = NULL;
 
-	tmp = malloc(sizeof(dlistint_t));
-	if (tmp == NULL)
-		return (NULL);
-	tmp->n = n;
-	tmp->next = *head;
-	tmp->prev = NULL;
-	if (*head != NULL)
-		(*head)->prev = tmp;
-	*head = tmp;
-	return (tmp);
+	if (*stack == NULL)
+	{
+		node->next = NULL;
+		*stack = node;
+	}
+	else
+	{
+		node->next = *stack;
+		(*stack)->prev = node;
+		*stack = node;
+	}
 }
